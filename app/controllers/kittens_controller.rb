c39f75484984b0d557fd5bf0691713a04f704075
crisandryj <crisandryj@gmail.com>
2022-11-04 08:41:08 -0400

    added all restful methods"

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,2 +1,38 @@
 class KittensController < ApplicationController
-end
+
+  def index
+    Kitten.all
+  end
+
+  def new
+    @kitten = Kitten.new()
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+      if @kitten.save
+          redirect_to root_path
+      end
+  end
+
+  def show
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def edit
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def update
+    if @kitten.update(kitten_params)
+    flash[:notice] = "Kitten was updated successfully"
+    redirect_to @kitten
+  else
+  render 'edit'
+  end
+
+
+  def destroy
+    @kitten.destroy
+    redirect_to root_path
+  end
